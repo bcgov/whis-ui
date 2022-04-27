979c07e451daee84c31ee27a0e25fc60b9c8dcf7
Robert Johnstone <rob@plasticviking.com>
2022-04-27 09:28:37 +0800

    dynamic import config (for caddy in openshift environment)

--- src/main/hooks/useAPI.ts
+++ src/main/hooks/useAPI.ts
@@ -1,15 +1,15 @@
-import {CONFIG} from "../../state/config";
-import {useReducer, useState} from "react";
 import {getAuthHeaders} from "../../state/utilities/authentication_helper";
 import {default as axios} from 'axios';
 import {useSelector} from "react-redux";
+import {getConfiguration} from "../../state/utilities/config_helper";
 
 export const useAPI = () => {
 	const authHeaders = useSelector(getAuthHeaders);
+	const configuration = useSelector(getConfiguration);
 
 	return {
 		getCodeTables:  async (): Promise<any> => {
-			const res = await axios.get(`${CONFIG.API_BASE}/codes`, {
+			const res = await axios.get(`${configuration.API_BASE}/codes`, {
 				headers: {
 					...authHeaders
 				}
@@ -17,7 +17,7 @@ export const useAPI = () => {
 			return res.data;
 		},
 		getCodeTable:  async (id: string): Promise<any> => {
-			const res = await axios.get(`${CONFIG.API_BASE}/codes/${id}`, {
+			const res = await axios.get(`${configuration.API_BASE}/codes/${id}`, {
 				headers: {
 					...authHeaders
 				}
@@ -25,7 +25,7 @@ export const useAPI = () => {
 			return res.data;
 		},
 		getHealthIDs:  async (): Promise<any> => {
-			const res = await axios.get(`${CONFIG.API_BASE}/ids`, {
+			const res = await axios.get(`${configuration.API_BASE}/ids`, {
 				headers: {
 					...authHeaders
 				}
