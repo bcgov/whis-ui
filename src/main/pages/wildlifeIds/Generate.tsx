b319b8d54f20ea2266b09caad0ff7bfebeaf9a77
Robert Johnstone <rob@plasticviking.com>
2022-09-30 17:07:22 +0800

    fixed up formatting

--- src/main/pages/wildlifeIds/Generate.tsx
+++ src/main/pages/wildlifeIds/Generate.tsx
@@ -8,16 +8,10 @@ import {
 	DialogActions,
 	DialogContent,
 	DialogTitle,
-	FormControl,
-	Grid,
 	IconButton,
 	InputAdornment,
-	InputLabel,
-	makeStyles,
 	MenuItem,
 	Paper,
-	Select,
-	Slider,
 	Stack,
 	TextField,
 	Typography
@@ -33,11 +27,12 @@ import {paperStyle} from "../../../state/style_constants";
 import CloseIcon from '@mui/icons-material/Close';
 import CheckIcon from '@mui/icons-material/Check';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-import { selectCodeTables } from "../../../state/reducers/code_tables";
+import {selectCodeTables} from "../../../state/reducers/code_tables";
 import Loading from "../../components/util/Loading";
-import { LockModal } from "../../components/wildlifeIds/LockModal";
-import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import {LockModal} from "../../components/wildlifeIds/LockModal";
+import {DatePicker, LocalizationProvider} from '@mui/x-date-pickers';
+import {AdapterDateFns} from '@mui/x-date-pickers/AdapterDateFns';
+
 const Generate: React.FC = () => {
 
 	const me = useSelector(state => state.Auth);
@@ -112,8 +107,8 @@ const Generate: React.FC = () => {
 	const handleUpdate = event => {
 		const currentState = formState;
 		switch (event.target.name) {
-		default:
-			currentState[event.target.name] = event.target.value;
+			default:
+				currentState[event.target.name] = event.target.value;
 		}
 		setFormState(currentState);
 		console.log(event.target.name + ":" + event.target.value);
@@ -149,43 +144,43 @@ const Generate: React.FC = () => {
 		const value = e.target.value;
 		if (value == "" || value == undefined || value == null) {
 			switch (name) {
-			case "year":
-				setinputState(state => ({...inputsState, year: true}));
-				break;
-			case "wlh_id":
-				setinputState(state => ({...inputsState, wlh_id: true}));
-				break;
-			case "purpose":
-				setinputState(state => ({...inputsState, purpose: true}));
-				break;
-			case "requesterFirstName":
-				setinputState(state => ({...inputsState, firstName: true}));
-				break;
-			case "requesterLastName":
-				setinputState(state => ({...inputsState, lastName: true}));
-				break;
-			default:
-				break;
+				case "year":
+					setinputState(state => ({...inputsState, year: true}));
+					break;
+				case "wlh_id":
+					setinputState(state => ({...inputsState, wlh_id: true}));
+					break;
+				case "purpose":
+					setinputState(state => ({...inputsState, purpose: true}));
+					break;
+				case "requesterFirstName":
+					setinputState(state => ({...inputsState, firstName: true}));
+					break;
+				case "requesterLastName":
+					setinputState(state => ({...inputsState, lastName: true}));
+					break;
+				default:
+					break;
 			}
 		} else {
 			switch (name) {
-			case "year":
-				setinputState(state => ({...inputsState, year: false}));
-				break;
-			case "wlh_id":
-				setinputState(state => ({...inputsState, wlh_id: false}));
-				break;
-			case "purpose":
-				setinputState(state => ({...inputsState, purpose: false}));
-				break;
-			case "requesterFirstName":
-				setinputState(state => ({...inputsState, firstName: false}));
-				break;
-			case "requesterLastName":
-				setinputState(state => ({...inputsState, lastName: false}));
-				break;
-			default:
-				break;
+				case "year":
+					setinputState(state => ({...inputsState, year: false}));
+					break;
+				case "wlh_id":
+					setinputState(state => ({...inputsState, wlh_id: false}));
+					break;
+				case "purpose":
+					setinputState(state => ({...inputsState, purpose: false}));
+					break;
+				case "requesterFirstName":
+					setinputState(state => ({...inputsState, firstName: false}));
+					break;
+				case "requesterLastName":
+					setinputState(state => ({...inputsState, lastName: false}));
+					break;
+				default:
+					break;
 			}
 		}
 	}
@@ -197,13 +192,13 @@ const Generate: React.FC = () => {
 
 	return (
 		<Paper sx={paperStyle}>
-			<LockModal open={lockModalOpen} />
+			<LockModal open={lockModalOpen}/>
 
 			<form onSubmit={handleRequiredSubmit}>
 
-				<Typography fontFamily={'BCSans-Bold'} sx={{ margin: '10px', fontSize: '32px' }}>Generate WLH ID</Typography>
-				<Typography sx={{ margin: '0 0 50px 12px' }}>Generate one or multiple WLH IDs by entering the information below.</Typography>
-				<hr />
+				<Typography fontFamily={'BCSans-Bold'} sx={{margin: '10px', fontSize: '32px'}}>Generate WLH ID</Typography>
+				<Typography sx={{margin: '0 0 50px 12px'}}>Generate one or multiple WLH IDs by entering the information below.</Typography>
+				<hr/>
 				<LocalizationProvider dateAdapter={AdapterDateFns}>
 					<TwoColumnForm title={'WLH ID information'}>
 
@@ -220,33 +215,39 @@ const Generate: React.FC = () => {
 							}}
 							renderInput={(params) =>
 								<TextField {...params}
-									sx={{ width: '100%' }}
-									placeholder='yyyy'
+													 sx={{width: '100%'}}
+													 placeholder='yyyy'
 								/>}
 						/>
 
 						<TextField
-							sx={{ width: '100%' }}
+							sx={{width: '100%'}}
 							label="Number of WLH IDs"
 							id="wlh_id"
 							name="wlh_id"
 							type="tel"
 							error={inputsState.wlh_id}
-							inputProps={{ maxLength: 3 }}
-							onChange={(e) => { handleOutofRange(e) }}
-							onBlur={(e) => { handleOnblur(e) }}
+							inputProps={{maxLength: 3}}
+							onChange={(e) => {
+								handleOutofRange(e)
+							}}
+							onBlur={(e) => {
+								handleOnblur(e)
+							}}
 							required
 						/>
 						<>
-							<TextField sx={{ width: '100%' }}
-								id="purpose"
-								name="purpose"
-								select
-								label="Purpose"
-								placeholder='Purpose*'
-								error={inputsState.purpose}
-								onBlur={(e) => { handleOnblur(e) }}
-								required
+							<TextField sx={{width: '100%'}}
+												 id="purpose"
+												 name="purpose"
+												 select
+												 label="Purpose"
+												 placeholder='Purpose*'
+												 error={inputsState.purpose}
+												 onBlur={(e) => {
+													 handleOnblur(e)
+												 }}
+												 required
 							>
 								{purposes.codes.map((m, i) => (
 									<MenuItem key={i} value={m.value}>
@@ -255,29 +256,29 @@ const Generate: React.FC = () => {
 								))}
 							</TextField>
 							<Dialog open={alertNumber} onClose={handleClose}
-								PaperProps={{
-									sx: { overflowY: 'inherit', width: '504px', height: '230px', borderRadius: '10px' }
-								}}
+											PaperProps={{
+												sx: {overflowY: 'inherit', width: '504px', height: '230px', borderRadius: '10px'}
+											}}
 							>
 								<DialogTitle fontFamily={'BCSans-Bold'}>Error</DialogTitle>
-								<DialogContent sx={{ margin: '40px auto', fontSize: '16px' }}>
+								<DialogContent sx={{margin: '40px auto', fontSize: '16px'}}>
 									Please enter a number between 1 - 100.
 								</DialogContent>
 								<DialogActions>
-									<Button variant={'contained'} onClick={handleClose} sx={{ width: '85px' }}>OK</Button>
-									<Button variant={'outlined'} onClick={handleClose} sx={{ width: '85px' }}>Cancel</Button>
+									<Button variant={'contained'} onClick={handleClose} sx={{width: '85px'}}>OK</Button>
+									<Button variant={'outlined'} onClick={handleClose} sx={{width: '85px'}}>Cancel</Button>
 								</DialogActions>
 							</Dialog>
 						</>
 
-						<TextField sx={{ width: '100%' }} label="Species"
-							id="species"
-							defaultValue={formState.species}
-							name="species"
-							onChange={handleUpdate}
-							InputProps={{
-								endAdornment: <InputAdornment position="end"><AccountTreeOutlinedIcon /></InputAdornment>,
-							}}
+						<TextField sx={{width: '100%'}} label="Species"
+											 id="species"
+											 defaultValue={formState.species}
+											 name="species"
+											 onChange={handleUpdate}
+											 InputProps={{
+												 endAdornment: <InputAdornment position="end"><AccountTreeOutlinedIcon/></InputAdornment>,
+											 }}
 						/>
 
 					</TwoColumnForm>
@@ -395,14 +396,14 @@ const Generate: React.FC = () => {
 					</TextField>
 				</TwoColumnForm>
 
-				<Box sx={{ position: 'relative' }}>
+				<Box sx={{position: 'relative'}}>
 					<Button
-						sx={{ display: OptionalButton ? 'auto' : 'none', textTransform: 'capitalize', position: 'absolute', top: '-60px', left: '35.5%', fontSize: '16px' }}
+						sx={{display: OptionalButton ? 'auto' : 'none', textTransform: 'capitalize', position: 'absolute', top: '-60px', left: '35.5%', fontSize: '16px'}}
 						onClick={() => {
 							setShowOptional(!showOptional);
 							setOptionalButton(!OptionalButton);
 						}} variant="outlined">
-						<AddIcon color='primary' />Requester Details (Optional)
+						<AddIcon color='primary'/>Requester Details (Optional)
 					</Button>
 				</Box>
 
@@ -410,10 +411,10 @@ const Generate: React.FC = () => {
 					open={open}
 					onClose={handleClose}
 					PaperProps={{
-						sx: { overflowY: 'inherit', width: '504px', height: '289px', borderRadius: '10px' }
+						sx: {overflowY: 'inherit', width: '504px', height: '289px', borderRadius: '10px'}
 					}}
 				>
-					<Box className='checkIcon'><CheckIcon sx={{ position: 'relative', top: '17px', left: '17px', fontSize: '45px', color: '#EEF2F6', }} /></Box>
+					<Box className='checkIcon'><CheckIcon sx={{position: 'relative', top: '17px', left: '17px', fontSize: '45px', color: '#EEF2F6',}}/></Box>
 					<IconButton
 						onClick={handleClose}
 						sx={{
@@ -424,23 +425,46 @@ const Generate: React.FC = () => {
 					>
 						<CloseIcon/>
 					</IconButton>
-					<DialogContent sx={{ margin: 'auto', padding: '0 24px', textAlign: 'center' }}>
-						<p style={{ color: '#666666', fontSize: '15px' }}>You have generated [N] WLH IDs: [number or range of numbers]</p>
-						<p style={{ color: '#666666', fontSize: '15px', marginTop: '25px' }}>Would you like to add more details to these IDs?</p>
+					<DialogContent sx={{margin: 'auto', padding: '0 24px', textAlign: 'center'}}>
+						<p style={{color: '#666666', fontSize: '15px'}}>You have generated [N] WLH IDs: [number or range of numbers]</p>
+						<p style={{color: '#666666', fontSize: '15px', marginTop: '25px'}}>Would you like to add more details to these IDs?</p>
 					</DialogContent>
-					<DialogActions sx={{ margin: 'auto', marginBottom: '25px' }}>
-						<Button onClick={() => { navigate('/wildlifeIds/list') }} sx={{ width: '110px', height: '42px', borderRadius: '6px', marginRight: '10px', backgroundColor: 'rgb(58, 219, 118)', color: '#fff', fontSize: '16px', ":hover": { backgroundColor: 'rgb(58, 219, 118)' } }}>YES</Button>
-						<Button onClick={() => { navigate('/wildlifeIds') }} sx={{ width: '110px', height: '42px', border: '1px solid rgb(134, 142, 150)', borderRadius: '6px', color: 'rgb(102, 102, 102)', fontSize: '16px', ":hover": { backgroundColor: '#fff' } }}>Later</Button>
+					<DialogActions sx={{margin: 'auto', marginBottom: '25px'}}>
+						<Button onClick={() => {
+							navigate('/wildlifeIds/list')
+						}}
+										sx={{
+											width: '110px',
+											height: '42px',
+											borderRadius: '6px',
+											marginRight: '10px',
+											backgroundColor: 'rgb(58, 219, 118)',
+											color: '#fff',
+											fontSize: '16px',
+											":hover": {backgroundColor: 'rgb(58, 219, 118)'}
+										}}>YES</Button>
+						<Button onClick={() => {
+							navigate('/wildlifeIds')
+						}}
+										sx={{
+											width: '110px',
+											height: '42px',
+											border: '1px solid rgb(134, 142, 150)',
+											borderRadius: '6px',
+											color: 'rgb(102, 102, 102)',
+											fontSize: '16px',
+											":hover": {backgroundColor: '#fff'}
+										}}>Later</Button>
 					</DialogActions>
 				</Dialog>
 
 
-				<hr style={{ 'marginBlock': '50px 40px' }} />
+				<hr style={{'marginBlock': '50px 40px'}}/>
 
-				<Stack spacing={2} direction={"row"} alignItems={'flex-end'} justifyContent={'flex-end'} sx={{ paddingRight: '5%', marginBottom: '30px' }}>
-					<GenerationLockWidget />
-					<Button type='submit' sx={{ textTransform: 'capitalize', padding: '9px 35px' }} variant={'contained'}>Generate</Button>
-					<Button sx={{ textTransform: 'capitalize', padding: '8px 28px' }} variant={'outlined'} onClick={() => {
+				<Stack spacing={2} direction={"row"} alignItems={'flex-end'} justifyContent={'flex-end'} sx={{paddingRight: '5%', marginBottom: '30px'}}>
+					<GenerationLockWidget/>
+					<Button type='submit' sx={{textTransform: 'capitalize', padding: '9px 35px'}} variant={'contained'}>Generate</Button>
+					<Button sx={{textTransform: 'capitalize', padding: '8px 28px'}} variant={'outlined'} onClick={() => {
 
 						navigate(-1)
 					}}>Cancel</Button>
