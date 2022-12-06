eafdefdf055de1e6e43814ca14be061581f0076e
Robert Johnstone <rob@plasticviking.com>
2022-12-06 16:31:13 +0800

    fix race

--- src/main/components/wildlifeIds/edit/EditForm.tsx
+++ src/main/components/wildlifeIds/edit/EditForm.tsx
@@ -146,7 +146,7 @@ const EditForm = ({wildlifeHealthId}) => {
 		return <Loading/>;
 	}
 
-	if (!formState.metadata?.wildlifeHealthId) {
+	if (!(formState?.metadata?.wildlifeHealthId)) {
 		return <Loading/>;
 	}
 
